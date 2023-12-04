cc66a0e75b33082790122b92c353eddeb248caa6
maktab1 <152245842+maktab1@users.noreply.github.com>
2023-12-04 22:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const cardsContainer = document.getElementById('cardsContainer');
+const prevButton = document.getElementById('prevButton');
+const nextButton = document.getElementById('nextButton');
 
 // Add your image filenames here
 const imageFilenames = [
@@ -42,56 +44,31 @@ document.querySelectorAll('.card').forEach((card) => {
     card.style.width = cardWidth;
 });
 
-// Function to handle touch events
-let touchStartX = 0;
-let touchEndX = 0;
-
-cardsContainer.addEventListener('touchstart', (e) => {
-    touchStartX = e.touches[0].clientX;
-});
-
-cardsContainer.addEventListener('touchend', (e) => {
-    touchEndX = e.changedTouches[0].clientX;
-    handleSwipe();
-});
-
-// Function to handle swipe gesture
-function handleSwipe() {
-    const swipeThreshold = 50; // Adjust as needed
-    const deltaX = touchEndX - touchStartX;
-
-    if (deltaX > swipeThreshold) {
-        // Swipe right (move to previous set of cards)
-        moveCards(1);
-    } else if (deltaX < -swipeThreshold) {
-        // Swipe left (move to next set of cards)
-        moveCards(-1);
-    }
-}
+let currentIndex = 0;
 
 // Function to move the cards container
-function moveCards(direction) {
-    const currentTransform = getComputedStyle(cardsContainer).transform;
-    const currentTranslateX = currentTransform !== 'none'
-        ? parseInt(currentTransform.split(',')[4].trim())
-        : 0;
-
-    const newTranslateX = currentTranslateX + direction * 100;
-
-    // Check if it's the first or last set of cards to create the loop effect
-    if (direction === 1 && newTranslateX > 0) {
-        cardsContainer.style.transition = 'none';
-        cardsContainer.style.transform = `translateX(${-imageFilenames.length * 100}%)`;
-        setTimeout(() => {
-            cardsContainer.style.transition = 'transform 0.5s ease';
-        }, 0);
-    } else if (direction === -1 && newTranslateX < -imageFilenames.length * 100) {
-        cardsContainer.style.transition = 'none';
-        cardsContainer.style.transform = 'translateX(0)';
-        setTimeout(() => {
-            cardsContainer.style.transition = 'transform 0.5s ease';
-        }, 0);
-    } else {
-        cardsContainer.style.transform = `translateX(${newTranslateX}%)`;
-    }
+function moveCards(index) {
+    currentIndex = index;
+    const newTranslateX = -currentIndex * 100;
+    cardsContainer.style.transform = `translateX(${newTranslateX}%)`;
 }
+
+// Function to handle automatic infinite loop
+function autoScroll() {
+    currentIndex = (currentIndex + 1) % imageFilenames.length;
+    moveCards(currentIndex);
+}
+
+// Set up automatic infinite loop
+setInterval(autoScroll, 5000); // Change the interval as needed (in milliseconds)
+
+// Event listeners for manual navigation buttons
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + imageFilenames.length) % imageFilenames.length;
+    moveCards(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % imageFilenames.length;
+    moveCards(currentIndex);
+});
