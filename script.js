c2252c7026dd04d299e6c6c09cdfab54781665e6
maktab1 <152245842+maktab1@users.noreply.github.com>
2023-12-04 22:45:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const cardsContainer = document.getElementById('cardsContainer');
 
 // Add your image filenames here
 const imageFilenames = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image5.jpg',
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.png',
+    '7.png',
+    '4.jpeg',
+    '2.jpeg',
+    '3.jpeg',
+    '4.jpeg',
+    'Maktab Icon.jpg',
     // Add more images as needed
 ];
 
